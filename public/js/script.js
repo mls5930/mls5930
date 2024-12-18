cc1079cb8b0d36935b3cc2300e6e8f7b01714c5b
mls <cjmadlife5930@gmail.com>
2024-12-18 18:01:19 +0900

    fix : spublic/js/script.js 파일 내용 수정

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,10 @@
 const red = document.querySelector(".red")
 
 const btn = document.createElement("button")
-// red.push(btn)
-// const button = (e) => {
-//     alert("버튼을 누르셨습니다 안녕하세요")
-// } 
+red.push(btn)
+const button = (e) => {
+    alert("버튼을 누르면 나오는 알렛 창을 수정하였습니다.")
+} 
 
-// btn.addEventListener("click",button)
+btn.addEventListener("click",button)
 
