21269fac765c3f129a8487768f3d38057fa29911
mls <cjmadlife5930@gmail.com>
2024-12-18 17:35:31 +0900

    feat : js 파일에 있는 버튼 클릭 이벤트 제거 develop

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,10 @@
 const red = document.querySelector(".red")
 
 const btn = document.createElement("button")
-red.push(btn)
-const button = (e) => {
-    alert("버튼을 누르셨습니다 안녕하세요")
-} 
+// red.push(btn)
+// const button = (e) => {
+//     alert("버튼을 누르셨습니다 안녕하세요")
+// } 
 
-btn.addEventListener("click",button)
+// btn.addEventListener("click",button)
 
