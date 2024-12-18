44be3a835732a9a522d4ca19863605f03f3e2d46
mls <cjmadlife5930@gmail.com>
2024-12-18 18:17:20 +0900

    feat: 최종 수정본

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,9 @@
 const red = document.querySelector(".red")
 
 const btn = document.createElement("button")
-red.push(btn)
+btn.innerHTML = "버튼을 눌러주세요"
+
+red.append(btn)
 const button = (e) => {
     alert("버튼을 누르면 나오는 알렛 창을 수정하였습니다.")
 } 
